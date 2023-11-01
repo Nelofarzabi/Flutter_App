4ce771d8b0c0e0dbb5a80fd6cbc75662fb2f0b73
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-11-01 07:15:37 +0430

    Add rows and clumns

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,65 @@
 import 'package:flutter/material.dart';
 
-void main(){
-runApp(MaterialApp(
-  home: Home(),
-));
+void main() {
+  runApp(MyApp());
 }
 
-class Home extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-  appBar: AppBar(title:Text(
-    'Flutter Application',
-      style: TextStyle(
-      color: Colors.white,
-    ),
-
-  ) , centerTitle: true, backgroundColor:Colors.brown[400],),
-     body: Center(child:Icon(Icons.access_alarm_outlined,)
-     ),
-  floatingActionButton: FloatingActionButton(onPressed: null , child:Text('Click') , backgroundColor: Colors.brown[300],),
- );
- }
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Basic Flutter Layout',) ,),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.orange,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.purple,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: null , child: Text('Click'),),
+      ),
+    );
+  }
 }
